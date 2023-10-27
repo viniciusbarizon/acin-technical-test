ca431a6f92139f82d455970608a14703291d9d87
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-27 20:08:57 +0100

    create customers table

---
+++ database/migrations/2023_10_27_190721_create_customers_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->string('email', 320)->unique();
+            $table->string('name', 45);
+            $table->biginteger('telephone')->unsigned();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
