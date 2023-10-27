e9bab89cee4356423ba7c91edce230c4ba36840e
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-27 22:11:43 +0100

    LoginAction

--- app/Actions/LoginAction.php
+++ app/Actions/LoginAction.php
@@ -1,8 +1,34 @@
 <?php
 
+namespace App\Actions;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class LoginAction
 {
-    public function login() {
+    private string $email;
+    private ?User $user;
+    private string $password;
 
+    public function login(string $email, string $password): string {
+        $this->email = $email;
+        $this->password = $password;
+
+        $this->setUser();
+
+        return $this->getToken();
+    }
+
+    private function setUser(): void {
+        $this->user = User::where('email', $this->email)->firstOrFail();
+    }
+
+    private function hashCheck(): bool {
+        return Hash::check($this->password, $this->user->password);
+    }
+
+    private function getToken(): string {
+        return $this->user->createToken(time())->plainTextToken;
     }
 }
