6b114e4cab1d79c5f92e5d2022be7862b6c4969c
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-27 23:38:36 +0100

    route logout

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login', LoginController::class);
+Route::post('/logout', LogoutController::class);
