765ee621472b3e92e0a594c916b8b90ee7bae28b
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-28 13:36:53 +0100

    create user_roles table

---
+++ database/migrations/2023_10_28_123103_create_user_roles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->ulid('id');
+            $table->primary('id');
+
+            $table->string('role', 20)->unique();
+
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_roles');
+    }
+};
