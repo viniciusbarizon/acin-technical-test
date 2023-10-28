db874369e606eafaeb7132b59813f1573b92c9e7
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-28 08:57:26 +0100

    InsertionActionTest

--- tests/Unit/InsertionActionTest.php
+++ tests/Unit/InsertionActionTest.php
@@ -13,46 +13,38 @@ class InsertionActionTest extends TestCase
 {
     use RefreshDatabase;
 
-    private string $model = Brand::class;
-    private array $brand;
-    private string $resource = BrandResource::class;
+    private Brand $brand;
+    private $response;
 
-    public function test_it_inserts_a_brand(): void
-    {
+    public function test_it_inserts_a_brand(): void {
         $this->setBrand();
+
+        $this->insert();
 
         $this->assertInserted();
     }
 
-    public function test_it_returns_http_code_conflict_if_brand_already_exists(): void
-    {
-        $this->brand = ['name' => $this->getName()];
+    public function test_it_returns_http_code_conflict_if_brand_already_exists(): void {
+        $this->brand = Brand::first();
 
-        $this->assertEquals($this->insert()->getStatusCode(), 409);
+        $this->insert();
+
+        $this->assertEquals($this->response->getStatusCode(), 409);
     }
 
     private function setBrand(): void {
-        $this->brand = Brand::factory()->make()->toArray();
+        $this->brand = Brand::factory()->make();
     }
 
-    private function getName(): string {
-        return Brand::first()->name;
-    }
-
-    private function insert(): mixed
-    {
-        return (new InsertionAction)->insert(
-            data: $this->brand,
+    private function insert(): void {
+        $this->response = (new InsertionAction)->insert(
+            data: $this->brand->toArray(),
             model: Brand::class,
             resource: BrandResource::class
         );
     }
 
-    private function assertInserted(): void
-    {
-        $this->assertDatabaseHas(
-            'brands',
-            ['name' => $this->brand['name']]
-        );
+    private function assertInserted(): void {
+        $this->assertDatabaseHas('brands', ['name' => $this->brand->name]);
     }
 }
