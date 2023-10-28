55cfd9a8ec6642ce20d0297c446c90ae42ac0f2e
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-28 17:14:36 +0100

    schedule weekly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->job(
             new SendWeekNewData()
-        )->everyMinute();
+        )->weekly();
     }
 
     /**
