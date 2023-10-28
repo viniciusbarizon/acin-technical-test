5c4daceadfb2a7ad02c6e62f81102e0e5cf98d81
Vinicius Barizon <viniciusbarizon@gmail.com>
2023-10-28 13:48:19 +0100

    UserRoleSeeder

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserRole;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('user_roles')
+            ->insert(['id' => Str::ulid(), 'role' => 'reporter']);
+
+        for ($i = 1; $i <= 9; $i++) {
+            UserRole::factory()->create();
+        }
+    }
+}
